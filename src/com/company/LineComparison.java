bedb88700499478ce5b4f33cfdf4c0a610364e6d
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-25 23:00:34 +0530

    Added : CompareTwoLines

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -6,7 +6,26 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            System.out.println("Welcome");
+            Scanner input = new Scanner(System.in);
+            int x1, x2, y1, y2;
+            System.out.println("Enter Coordinates x1,y1,x2,y2");
+            x1 = input.nextInt();
+            y1 = input.nextInt();
+            x2 = input.nextInt();
+            y2 = input.nextInt();
+            double Length =Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) );
+            System.out.println(Length);
+            System.out.println("Your length is " + Length);
+            System.out.println(compare(x1,x2,y1,y2));
+            if( x1==y1 && x2==y2){
+                System.out.println("Two Line are qualified");
+            }else{
+                System.out.println("Not qualified");
+            }
+        }
+
+        private static int compare(int x1, int x2, int y1, int y2) {
+            return 0;
         }
 
     }

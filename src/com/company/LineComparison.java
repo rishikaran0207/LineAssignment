b340fb023f208755bcbf2cd35e05d4605a3e721d
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-25 22:34:03 +0530

    LineComparison

---
+++ src/com/company/LineComparison.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.Scanner;
+
+    public class LineComparison {
+
+        public static void main(String[] args) {
+
+            System.out.println("Welcome");
+        }
+
+    }
+

fdc07ff31c7e72ccc9afc62e179ddf11e92f2956
rishikaran0207 <sendittomemrk@gmail.com>
2021-08-25 22:39:56 +0530

    Added: ModelXY

--- src/com/company/LineComparison.java
+++ src/com/company/LineComparison.java
@@ -6,7 +6,16 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            System.out.println("Welcome");
+            Scanner input = new Scanner(System.in);
+            int x1, x2, y1, y2;
+            System.out.println("Enter Coordinates x1,y1,x2,y2");
+            x1 = input.nextInt();
+            y1 = input.nextInt();
+            x2 = input.nextInt();
+            y2 = input.nextInt();
+            double Length =Math.sqrt(Math.pow( (x2-x1) ,2 ) + Math.pow( (y2-y1) , 2) );
+            System.out.println(Length);
+            System.out.println("Your length is " + Length);
         }
 
     }
